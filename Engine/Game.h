ba4353da8916bb02e70a7fa345e0bc7a2f6f0494
Garrett <gman9mm@live.com>
2021-06-09 15:17:00 -0700

    Add DrawGameOver function and contents.

--- Engine/Game.h
+++ Engine/Game.h
@@ -39,6 +39,7 @@ private:
 	/*  User Functions              */
 	void DrawFace(int x, int y);
 	void DrawPoo(int x, int y);
+	void DrawGameOver(int x, int y);
 	int ClampScreenX(int x, int width);
 	int ClampScreenY(int y, int height);
 	bool IsColliding(int x0, int y0, int width0, int height0, int x1, int y1, int width1, int height1);
