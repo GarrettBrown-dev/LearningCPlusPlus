4c3e370ea99a73ef86cfdc1860e011c801b5526f
Garrett <gman9mm@live.com>
2021-06-09 13:48:51 -0700

    Wiping all code from previous tutorials for a new game!!

--- Engine/Game.h
+++ Engine/Game.h
@@ -37,26 +37,11 @@ private:
 	void UpdateModel();
 	/********************************/
 	/*  User Functions              */
-	void DrawBox(int x, int y, int r, int g, int b);
-	bool OverlapTest(int box0x, int box0y, int box1x, int box1y);
-	int ClampScreenX(int x);
-	int ClampScreenY(int y);
 	/********************************/
 private:
 	MainWindow& wnd; //Data members
 	Graphics gfx;
 	/********************************/
 	/*  User Variables              */
-	int x_fixed0 = 200;
-	int y_fixed0 = 200;
-	int x_fixed1 = 210;
-	int y_fixed1 = 200;
-	int x_fixed2 = 215;
-	int y_fixed2 = 200;
-	int x_fixed3 = 230;
-	int y_fixed3 = 200;
-	int x_mobile = 400;
-	int y_mobile = 300;
-	int colliding = false;
 	/********************************/
 };
