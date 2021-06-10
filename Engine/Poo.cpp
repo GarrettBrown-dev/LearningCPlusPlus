dd9f736b4410c672fa8463c6fe2e04c4ff956b84
Garrett <gman9mm@live.com>
2021-06-10 15:19:54 -0700

    Add rebound physics to Poo.cpp.

--- Engine/Poo.cpp
+++ Engine/Poo.cpp
@@ -10,19 +10,23 @@ void Poo::Update()
 	if (x < 0)
 	{
 		x = 0;
+		vx = -vx;
 	}
 	else if (right >= Graphics::ScreenWidth)
 	{
 		x = (Graphics::ScreenWidth - 1) - width;
+		vx = -vx;
 	}
 
 	const int bottom = y + height;
 	if (y < 0)
 	{
 		y = 0;
+		vy = -vy;
 	}
 	else if (bottom >= Graphics::ScreenHeight)
 	{
 		y = (Graphics::ScreenHeight - 1) - height;
+		vy = -vy;
 	}
 }
